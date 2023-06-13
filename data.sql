d67473622bb2e492e7992698aaee81c50c282acd
= <mirfarahi92@gmail.com>
2023-06-13 09:29:38 +0430

    Edit data.sql File

---
+++ data.sql
@@ -0,0 +1,6 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals(date_of_birth, escape_attempts, neutered, weight_kg, name) VALUES ('02/03/2020', 0, true, 10.23, 'Agumon');
+INSERT INTO animals(date_of_birth, escape_attempts, neutered, weight_kg, name) VALUES ('15/11/2018', 2, true, 8, 'Gabumon');
+INSERT INTO animals(date_of_birth, escape_attempts, neutered, weight_kg, name) VALUES ('07/01/2021', 1, false, 15.04, 'Pikachu');
+INSERT INTO animals(date_of_birth, escape_attempts, neutered, weight_kg, name) VALUES ('12/05/2017', 5, true, 11, 'Devimon');
