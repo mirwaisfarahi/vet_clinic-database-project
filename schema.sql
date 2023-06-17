c56657b26e638100e64e4ca74ab63c0b66eedc3f
= <mirfarahi92@gmail.com>
2023-06-17 11:32:24 +0430

    Edit schema.sql File

--- schema.sql
+++ schema.sql
@@ -7,3 +7,10 @@ CREATE TABLE animals
 
 -- Query and update animals table
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+-- Create owners and species tables and alter animals table.
+CREATE TABLE owners(id serial PRIMARY KEY, full_name VARCHAR(50) NOT NULL, age INT NOT NULL);
+CREATE TABLE species(id serial PRIMARY KEY, name VARCHAR(50) NOT NULL);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
